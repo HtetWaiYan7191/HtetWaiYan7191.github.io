8e439d33e52c448ee276c30dc094802a20223c8a
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-05-11 12:08:57 +0630

    Add mobile menu bar by using js script

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const input = document.querySelector('input[type="text"]');
+const navbar = document.getElementById('nav-bar');
+const menuNavbar = document.getElementById('menu-navbar');
+const crossBtn = document.getElementById('cross-btn');
 
 input.addEventListener('focus', (e) => {
   e.target.style.backgroundColor = 'black';
@@ -8,4 +11,16 @@ input.addEventListener('focus', (e) => {
 input.addEventListener('blur', (e) => {
   e.target.style.backgroundColor = '';
   e.target.style.color = '';
+});
+
+navbar.addEventListener('click', () => {
+  if (menuNavbar.style.display === 'none') {
+    menuNavbar.style.display = 'block';
+  } else {
+    menuNavbar.style.display = 'none';
+  }
+});
+
+crossBtn.addEventListener('click', () => {
+  menuNavbar.style.display = 'none';
 });
