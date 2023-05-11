8155480371f824bf1c2841b5232dedc8ee3d1566
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-05-11 11:14:57 +0630

    Add js to style form input when focus

---
+++ app.js
@@ -0,0 +1,11 @@
+let input = document.querySelector('input[type="text"]');
+
+input.addEventListener('focus',(e)=>{
+    e.target.style.backgroundColor = "black";
+    e.target.style.color = "#FFFFFF";
+})
+
+input.addEventListener('blur', (e) => {
+    e.target.style.backgroundColor = "";
+    e.target.style.color = "";
+})
