7c66cc4a25417df546feee9c67adf93877cc40dd
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-05-11 11:43:42 +0630

    Add linter for javascript

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-let input = document.querySelector('input[type="text"]');
+const input = document.querySelector('input[type="text"]');
 
-input.addEventListener('focus',(e)=>{
-    e.target.style.backgroundColor = "black";
-    e.target.style.color = "#FFFFFF";
-})
+input.addEventListener('focus', (e) => {
+  e.target.style.backgroundColor = 'black';
+  e.target.style.color = '#FFFFFF';
+});
 
 input.addEventListener('blur', (e) => {
-    e.target.style.backgroundColor = "";
-    e.target.style.color = "";
-})
+  e.target.style.backgroundColor = '';
+  e.target.style.color = '';
+});
