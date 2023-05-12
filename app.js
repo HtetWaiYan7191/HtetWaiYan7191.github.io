9d315acef9d7f79705420676d819a0888289dc3c
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-05-12 15:56:40 +0630

    Update mobile menu bar

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const input = document.querySelector('input[type="text"]');
 const navbar = document.getElementById('nav-bar');
 const menuNavbar = document.getElementById('menu-navbar');
 const crossBtn = document.getElementById('cross-btn');
+const lists = document.querySelectorAll('#menu-navbar li');
+
+lists.forEach((list) => {
+  list.addEventListener('click', () => {
+    menuNavbar.style.display = 'none';
+  });
+});
 
 input.addEventListener('focus', (e) => {
   e.target.style.backgroundColor = 'black';
