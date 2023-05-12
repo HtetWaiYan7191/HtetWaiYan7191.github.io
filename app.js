9f6594af400680f5f25dd9613925a3501b0e8c5e
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-05-12 22:05:02 +0630

    Add simple pop-up modal

--- app.js
+++ app.js
@@ -3,6 +3,35 @@ const navbar = document.getElementById('nav-bar');
 const menuNavbar = document.getElementById('menu-navbar');
 const crossBtn = document.getElementById('cross-btn');
 const lists = document.querySelectorAll('#menu-navbar li');
+const openPopButtons = document.querySelectorAll('[data-modal-target]');
+const closePopButtons = document.querySelectorAll('[data-close-button');
+const overlay = document.getElementById('overlay');
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add('active');
+  overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+}
+
+openPopButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  });
+});
+
+closePopButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal');
+    closeModal(modal);
+  });
+});
 
 lists.forEach((list) => {
   list.addEventListener('click', () => {
